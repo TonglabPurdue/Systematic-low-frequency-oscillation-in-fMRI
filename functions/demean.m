7069b19a1d467550479ac1b0f08a04e808f6f544
Jinxia Yao <fionayjx27@gmail.com>
2018-09-27 10:43:27 -0400

    Update demean.m

--- functions/demean.m
+++ functions/demean.m
@@ -1,2 +1,10 @@
+% (C) Copyright 2018                TonglabPurdue
+% All rights reserved               Weldon School of Biomedical Engineering
+%                                   Purdue University
+%                                   West Lafayette, Indiana
+%                                   USA
+% Yunjie Tong, 2012
+
+
 function [demean_ts]=demean(ts)
 demean_ts=detrend(ts)/std(detrend(ts));
