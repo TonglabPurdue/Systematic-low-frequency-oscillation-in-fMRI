1c453f1f84d597b61dfb62e1c7d304242227c987
photoacoustic <32625580+photoacoustic@users.noreply.github.com>
2018-09-27 10:40:26 -0400

    Update filtf.m

--- functions/filtf.m
+++ functions/filtf.m
@@ -1,3 +1,10 @@
+% (C) Copyright 2018                TonglabPurdue
+% All rights reserved               Weldon School of Biomedical Engineering
+%                                   Purdue University
+%                                   West Lafayette, Indiana
+%                                   USA
+% Yunjie Tong, 2012
+
 function y=filtf(input_signal,lpass, hpass,freq)
 L=length(input_signal);
 N=floor(L/10);
